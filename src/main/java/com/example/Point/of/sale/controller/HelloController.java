cac2323f496cd48dfb952c29719f92a4099c9600
Zakari-Anas <111804421+Zakari-Anas@users.noreply.github.com>
2024-01-04 19:39:50 +0100

    Update HelloController.java

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from spring boot application Zakari :)";
+        return "Hello from spring boot application Zakariiii :)";
     }
 
     @GetMapping("/user")
